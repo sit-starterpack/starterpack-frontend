d793eedf78baa714355d94c3d8e4da377e8dea69
Meawmfia <boon20002543@hotmail.com>
2021-07-21 20:53:42 +0700

    [Add] button , responsive button

--- store/index.js
+++ store/index.js
@@ -1,1 +1,15 @@
-export const state = () => ({});
+export const state = {
+  id: 0,
+};
+
+export const mutations = {
+  UPDATE_ID(state, payload) {
+    state.id = payload;
+  },
+};
+
+export const actions = {
+  addId(context, value) {
+    context.commit('UPDATE_ID', value);
+  },
+};
